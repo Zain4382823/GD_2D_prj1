0774cbbb8dab41ae787b33f736391561edecf189
Zain4382823 <zain.harith@yahoo.com>
2025-03-24 11:28:22 +0000

    Still on WK4 PT 1 STEP 1
    We're currently at this point
    
    "As mentioned in the lecture, the constructor tells us how to build an object, but in Unreal, we also add an interface for us to work with.
    Not for the collider use this to help set up:
    https://dev.epicgames.com/documentation/en-us/unreal-engine/API/Runtime/Engine/Components/UBoxComponent
    You don’t want to replace the RootComponent, but you can attach a child object like this:
    YourCollider->SetupAttachment(RootComponent);"
    
    This part is quite confusing. We'll figure it all out tomorrow when we're at the Dalton Building / Library.

---
+++ Source/GD_2D_prj1/Private/BreakableScenery.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BreakableScenery.h"
+
+// Sets default values
+ABreakableScenery::ABreakableScenery()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//PrimaryActorTick.bCanEverTick = true;
+
+	// Create the sprite component
+	Sprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("SpriteComponent"));
+	RootComponent = Sprite;
+}
+
+// Called when the game starts or when spawned
+void ABreakableScenery::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+//void ABreakableScenery::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+
---
+++ Source/GD_2D_prj1/Public/BreakableScenery.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PaperSpriteComponent.h"
+#include "BreakableScenery.generated.h"
+
+UCLASS()
+class GD_2D_PRJ1_API ABreakableScenery : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABreakableScenery();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// the objects sprite
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visuals")
+	UPaperSpriteComponent* Sprite;
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
